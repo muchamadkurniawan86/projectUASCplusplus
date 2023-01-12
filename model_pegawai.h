3f29901661fbda289698a0f9cd78d237304585bf
INTEL-NUC <dept.if@itats.ac.id>
2023-01-12 18:31:26 +0700

    initial Commit

---
+++ model_pegawai.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void insert();
+
+void update();
+
+void select();
+
+void del();
+
+void selectById();
---
+++ storage.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int id [100];
+string nama [10];
+string pass [10];
+string role [10];
+bool status[10];
+
